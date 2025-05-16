582ec23b54872ea2e7c05343da595ba566e86f16
Ionut Opria <opriaionut14@gmail.com>
2025-05-16 17:10:38 +0300

    Made the phoenix flame animation

--- src/client/scripts/PhoenixFlame/PhoenixFlameScene.ts
+++ src/client/scripts/PhoenixFlame/PhoenixFlameScene.ts
@@ -1,10 +1,24 @@
+import { AnimatedSprite, Assets, Spritesheet, ISpritesheetData } from "pixi.js";
 import { Scene } from "../Utils/Scene";
 
 export class PhoenixFlameScene extends Scene
 {
-    public start(): void 
+    public async start() 
     {
-        
+        let spritesheetData = await Assets.load('/config/spritesheet.json');
+
+        const flameData: ISpritesheetData = spritesheetData.flame;
+
+        const atlasTexture = await Assets.load(flameData.meta.image as string);
+        const spritesheet = new Spritesheet(atlasTexture, flameData);
+        await spritesheet.parse();
+
+        const animatedSprite = new AnimatedSprite(spritesheet.animations.flame)
+        animatedSprite.anchor.set(0.5, 0.5);
+        animatedSprite.position.set(window.innerWidth / 2, window.innerHeight / 2);
+        animatedSprite.animationSpeed = 0.15
+        animatedSprite.play();
+        this.scene.addChild(animatedSprite);
     }
 
     public update(): void 
